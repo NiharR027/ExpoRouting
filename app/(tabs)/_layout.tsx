3b45b0fd13d3ea2b9a3698bc8a5cd6db1fe0dbd7
NiharR027 <nihar@conpago.com.au>
2025-03-29 13:39:17 +1000

    feat: show tab bar icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,49 @@
 // app/(tabs)/_layout.tsx
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ title: "Home", headerShown: false }}
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="chat"
-        options={{ title: "Chat", headerShown: false }}
+        options={{
+          title: "Chat",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="chatbubble-outline" size={size} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="newsfeed"
-        options={{ title: "Newsfeed", headerShown: false }}
+        options={{
+          title: "Newsfeed",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="newspaper-outline" size={size} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="notifications"
-        options={{ title: "Notifications", headerShown: false }}
+        options={{
+          title: "Notifications",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="notifications-outline" size={size} color={color} />
+          ),
+        }}
       />
     </Tabs>
   );
